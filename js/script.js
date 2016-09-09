9c0603abc3c580b8220ec992efcd4470ae26d193
Luke <luke.rooney@me.com>
2016-09-09 00:02:41 -0700

    added grid change dimension and random RGB mode

--- js/script.js
+++ js/script.js
@@ -1,16 +1,58 @@
 $(document).ready(function() {
-	// creates a 16x16 
-	var numCols = 16, numRows = 16;
-	for(var r=1; r<=numRows; r++) {
-		var $row = "<tr id='row "+r+"'>";
-		for(var c=1; c<=numCols; c++) {
-			$row += "<td class='box'></td>";
+	const HEIGHT = 960;
+	const WIDTH = 960;
+
+	var side = 16;
+	var randomRGBMode = false;
+
+	$("#grid").css({"width":  WIDTH, "height": HEIGHT});
+
+	function generateGrid(sideLength) {
+		for(var r=1; r<=sideLength; r++) {
+			var $row = "<tr id='row "+r+"'>";
+			for(var c=1; c<=sideLength; c++) {
+				$row += "<td class='box'></td>";
+			}
+			$row += "</tr>";
+			$("#grid table tbody").append($row);
 		}
-		$row += "</tr>";
-		$("#grid table tbody").append($row);
+		$(".box").css({"height": (HEIGHT/sideLength)+"px", "width": (WIDTH/sideLength)+"px"});
 	}
 
-	$("td").hover(function() {
-		$(this).addClass("fill");
+	function clear() {
+		console.log("clear is called");
+		$("td").removeClass("fill");
+		$("tbody tr").remove();
+		var size = prompt("How many squares per side would you like to make the new grid?");
+		generateGrid(size);
+	}
+
+	function randomRGB() {
+		return "rgb("+Math.floor(Math.random() * 256)+
+				  ","+Math.floor(Math.random() * 256)+
+				  ","+Math.floor(Math.random() * 256)+")";
+	}
+
+	generateGrid(side);
+
+	$(document).on({
+    	mouseenter: function () {
+    		if(randomRGBMode) {
+    			$(this).css("background-color", randomRGB());
+    		} else {
+    			$(this).addClass("fill");	
+    		} 	
+    	}
+	}, 'td');
+	
+
+	$("#clear-btn").click(function() {
+		clear();		
+	});		
+		
+
+	$("#random-btn").click(function() {
+		randomRGBMode = !randomRGBMode;
+		clear();
 	});
 });
